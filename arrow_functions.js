10d3293b59c18e0b597bcb8c57f72c4bbc408ddc
cobeliii <cobeliiimuname@gmail.com>
2025-02-21 00:30:41 -0600

    2 new files added and functions.js modified

---
+++ arrow_functions.js
@@ -0,0 +1,5 @@
+const getData = (name = "Jamila", age = 29, email = "a@abc.com") => 
+    console.log(`${name}, ${age}, ${email}`)
+
+
+getData();
--- functions.js
+++ functions.js
@@ -2,3 +2,16 @@ function greeting(){
     console.log("Hello there")
 }
 
+function greetingWithName(name){
+    console.log(`Hello, ${name}`)
+}
+
+function getData(){
+    return ['Jamila', 28]
+}
+
+let [name, age] = getData()
+
+console.log(`${name}, ${age}`)
+greeting()
+greetingWithName("John")
